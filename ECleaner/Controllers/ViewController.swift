94cf8c32bb7a8e7f29be4f0e68d022fecc59ad55
‘oleksiiswift’ <oleksii.codingKey@gmail.com>
2021-06-22 15:43:46 +0300

    EC-4 adding video sort and find for duplicates

--- ECleaner/Controllers/ViewController.swift
+++ ECleaner/Controllers/ViewController.swift
@@ -10,16 +10,27 @@ import PhotosUI
 
 class ViewController: UIViewController {
     
+//
+    
     var assetCollection: PHAssetCollection!
-
+    
+//    var delegate: PhotoManagerDelegate?
+    
+    @IBOutlet weak var temp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PhotoManager.manager.calculateSpace { space in
-            debugPrint("->>>>>>>>")
-            debugPrint(space)
-            debugPrint("<<<<<<<<-")
-        }
+//        PhotoManager.manager.calculateSpace { space in
+//            debugPrint("->>>>>>>>")
+//            debugPrint(space)
+//            debugPrint("<<<<<<<<-")
+//        }
+        
+//        PhotoManager().delegate = self
+        
+        
+//        delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -29,6 +40,12 @@ class ViewController: UIViewController {
             PhotoManager.manager.getPhotoLibrary()
         }
     }
-    
-
 }
+
+//extension ViewController: PhotoManagerDelegate {
+//    func filesCountProcessing(count: Int) {
+//        temp.text = String(count)
+//    }
+//
+//
+//}
