f1f04822d4bf4f6bdc697829b582a1f9c66d2aba
‘oleksiiswift’ <oleksii.codingKey@gmail.com>
2021-06-24 01:11:22 +0300

    EC-5 add testing progress bar

--- ECleaner/Controllers/MainViewController.swift
+++ ECleaner/Controllers/MainViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class MainViewController: UIViewController {
    
     
+    @IBOutlet weak var progressBarview: MKMagneticProgress!
     @IBOutlet weak var mediaCollectionViewContainer: UIView!
     @IBOutlet weak var mediaCollectionView: UICollectionView!
     @IBOutlet weak var deepCleaningButtonView: UIView!
@@ -32,6 +33,31 @@ class MainViewController: UIViewController {
         setupUI()
         setupCollectionView()
         updateColors()
+        
+        
+
+        let used = UIDevice.current.usedDiskSpaceInGB
+        let total = UIDevice.current.totalDiskSpaceInGB
+        let percentage: Double = Double(UIDevice.current.usedDiskSpaceInBytes) / Double(UIDevice.current.totalDiskSpaceInBytes)
+        
+       
+//
+        
+        
+        progressBarview.setProgress(progress: CGFloat(percentage), animated: true)
+        progressBarview.progressShapeColor = currentTheme.tintColor
+        progressBarview.backgroundShapeColor = currentTheme.contentBackgroundColor
+        progressBarview.titleColor = UIColor.red
+        progressBarview.percentColor = UIColor.black
+
+        progressBarview.lineWidth = 10
+        progressBarview.orientation = .bottom
+        progressBarview.lineCap = .round
+        progressBarview.clockwise = true
+
+        progressBarview.title = "\(used) out of \(total)"
+        progressBarview.largeContentTitle = "hello"
+        progressBarview.percentLabelFormat = "%.2f%%"
     }
 }
 
@@ -140,4 +166,451 @@ extension MainViewController: UpdateColorsDelegate {
     }
 }
 
+public enum LineCap : Int{
+    case round, butt, square
+    
+    public func style() -> String {
+        switch self {
+        case .round:
+            return CAShapeLayerLineCap.round.rawValue
+        case .butt:
+            return CAShapeLayerLineCap.butt.rawValue
+        case .square:
+            return CAShapeLayerLineCap.square.rawValue
+        }
+    }
+}
 
+// MARK: - Orientation Enum
+public enum Orientation: Int  {
+    case left, top, right, bottom
+    
+}
+
+@IBDesignable
+open class MKMagneticProgress: UIView {
+    
+    // MARK: - Variables
+    private let titleLabelWidth:CGFloat = 100
+    
+    private let percentLabel = UILabel(frame: .zero)
+    @IBInspectable open var titleLabel = UILabel(frame: .zero)
+    
+    /// Stroke background color
+    @IBInspectable open var clockwise: Bool = true {
+        didSet {
+            layoutSubviews()
+        }
+    }
+    
+    /// Stroke background color
+    @IBInspectable open var backgroundShapeColor: UIColor = UIColor(white: 0.9, alpha: 0.5) {
+        didSet {
+            updateShapes()
+        }
+    }
+    
+    /// Progress stroke color
+    @IBInspectable open var progressShapeColor: UIColor   = .blue {
+        didSet {
+            updateShapes()
+        }
+    }
+    
+    /// Line width
+    @IBInspectable open var lineWidth: CGFloat = 8.0 {
+        didSet {
+            updateShapes()
+        }
+    }
+    
+    /// Space value
+    @IBInspectable open var spaceDegree: CGFloat = 45.0 {
+        didSet {
+//            if spaceDegree < 45.0{
+//                spaceDegree = 45.0
+//            }
+//
+//            if spaceDegree > 135.0{
+//                spaceDegree = 135.0
+//            }
+            
+            layoutSubviews()
+
+            updateShapes()
+        }
+    }
+    
+    /// The progress shapes line width will be the `line width` minus the `inset`.
+    @IBInspectable open var inset: CGFloat = 0.0 {
+        didSet {
+            updateShapes()
+        }
+    }
+    
+    // The progress percentage label(center label) format
+    @IBInspectable open var percentLabelFormat: String = "%.f %%" {
+        didSet {
+            percentLabel.text = String(format: percentLabelFormat, progress * 100)
+        }
+    }
+    
+    @IBInspectable open var percentColor: UIColor = UIColor(white: 0.9, alpha: 0.5) {
+        didSet {
+            percentLabel.textColor = percentColor
+        }
+    }
+    
+    
+    /// progress text (progress bottom label)
+    @IBInspectable open var title: String = "" {
+        didSet {
+            titleLabel.text = title
+        }
+    }
+    
+    @IBInspectable open var titleColor: UIColor = UIColor(white: 0.9, alpha: 0.5) {
+        didSet {
+            titleLabel.textColor = titleColor
+        }
+    }
+    
+    
+    // progress text (progress bottom label)
+    @IBInspectable  open var font: UIFont = .systemFont(ofSize: 13) {
+        didSet {
+            titleLabel.font = font
+            percentLabel.font = font
+        }
+    }
+    
+    
+    // progress Orientation
+    open var orientation: Orientation = .bottom {
+        didSet {
+            updateShapes()
+        }
+    }
+
+    /// Progress shapes line cap.
+    open var lineCap: LineCap = .round {
+        didSet {
+            updateShapes()
+        }
+    }
+    
+    /// Returns the current progress.
+    @IBInspectable open private(set) var progress: CGFloat {
+        set {
+            progressShape?.strokeEnd = newValue
+        }
+        get {
+            return progressShape.strokeEnd
+        }
+    }
+    
+    /// Duration for a complete animation from 0.0 to 1.0.
+    open var completeDuration: Double = 2.0
+    
+    private var backgroundShape: CAShapeLayer!
+    private var progressShape: CAShapeLayer!
+    
+    private var progressAnimation: CABasicAnimation!
+    
+    // MARK: - Init
+    
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    private func setup() {
+        
+        backgroundShape = CAShapeLayer()
+        backgroundShape.fillColor = nil
+        backgroundShape.strokeColor = backgroundShapeColor.cgColor
+        layer.addSublayer(backgroundShape)
+        
+        progressShape = CAShapeLayer()
+        progressShape.fillColor   = nil
+        progressShape.strokeStart = 0.0
+        progressShape.strokeEnd   = 0.1
+        layer.addSublayer(progressShape)
+        
+        progressAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        
+        percentLabel.frame = self.bounds
+        percentLabel.textAlignment = .center
+//        percentLabel.textColor = self.progressShapeColor
+        self.addSubview(percentLabel)
+        percentLabel.text = String(format: "%.1f%%", progress * 100)
+        
+        
+        titleLabel.frame = CGRect(x: (self.bounds.size.width-titleLabelWidth)/2, y: self.bounds.size.height-21, width: titleLabelWidth, height: 21)
+        
+        titleLabel.textAlignment = .center
+//        titleLabel.textColor = self.progressShapeColor
+        titleLabel.text = title
+        titleLabel.contentScaleFactor = 0.3
+        //        textLabel.adjustFontSizeToFit()
+        titleLabel.numberOfLines = 2
+        
+        //textLabel.adjustFontSizeToFit()
+        titleLabel.adjustsFontSizeToFitWidth = true
+        self.addSubview(titleLabel)
+    }
+    
+    // MARK: - Progress Animation
+    
+    public func setProgress(progress: CGFloat, animated: Bool = true) {
+        
+        if progress > 1.0 {
+            return
+        }
+        
+        var start = progressShape.strokeEnd
+        if let presentationLayer = progressShape.presentation(){
+            if let count = progressShape.animationKeys()?.count, count > 0  {
+            start = presentationLayer.strokeEnd
+            }
+        }
+        
+        let duration = abs(Double(progress - start)) * completeDuration
+        percentLabel.text = String(format: percentLabelFormat, progress * 100)
+        progressShape.strokeEnd = progress
+        
+        if animated {
+            progressAnimation.fromValue = start
+            progressAnimation.toValue   = progress
+            progressAnimation.duration  = duration
+            progressShape.add(progressAnimation, forKey: progressAnimation.keyPath)
+        }
+    }
+    
+    // MARK: - Layout
+    
+    open override func layoutSubviews() {
+        
+        super.layoutSubviews()
+        
+        backgroundShape.frame = bounds
+        progressShape.frame   = bounds
+        
+        let rect = rectForShape()
+        backgroundShape.path = pathForShape(rect: rect).cgPath
+        progressShape.path   = pathForShape(rect: rect).cgPath
+        
+        self.titleLabel.frame = CGRect(x: (self.bounds.size.width - titleLabelWidth)/2, y: self.bounds.size.height-50, width: titleLabelWidth, height: 42)
+        
+        updateShapes()
+        
+        percentLabel.frame = self.bounds
+    }
+    
+    private func updateShapes() {
+        backgroundShape?.lineWidth  = lineWidth
+        backgroundShape?.strokeColor = backgroundShapeColor.cgColor
+        backgroundShape?.lineCap     = CAShapeLayerLineCap(rawValue: lineCap.style())
+        
+        progressShape?.strokeColor = progressShapeColor.cgColor
+        progressShape?.lineWidth   = lineWidth - inset
+        progressShape?.lineCap     = CAShapeLayerLineCap(rawValue: lineCap.style())
+        
+        switch orientation {
+        case .left:
+            titleLabel.isHidden = true
+            self.progressShape.transform = CATransform3DMakeRotation( CGFloat.pi / 2, 0, 0, 1.0)
+            self.backgroundShape.transform = CATransform3DMakeRotation(CGFloat.pi / 2, 0, 0, 1.0)
+        case .right:
+            titleLabel.isHidden = true
+            self.progressShape.transform = CATransform3DMakeRotation( CGFloat.pi * 1.5, 0, 0, 1.0)
+            self.backgroundShape.transform = CATransform3DMakeRotation(CGFloat.pi * 1.5, 0, 0, 1.0)
+        case .bottom:
+            titleLabel.isHidden = false
+            UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: [] , animations: { [weak self] in
+                if let temp = self{
+                    temp.titleLabel.frame = CGRect(x: (temp.bounds.size.width - temp.titleLabelWidth)/2, y: temp.bounds.size.height-50, width: temp.titleLabelWidth, height: 42)
+                }
+
+            }, completion: nil)
+            self.progressShape.transform = CATransform3DMakeRotation( CGFloat.pi * 2, 0, 0, 1.0)
+            self.backgroundShape.transform = CATransform3DMakeRotation(CGFloat.pi * 2, 0, 0, 1.0)
+        case .top:
+            titleLabel.isHidden = false
+            UIView.animate(withDuration: 0.3, delay: 0.0, usingSpringWithDamping: 0.9, initialSpringVelocity: 0.0, options: [] , animations: { [weak self] in
+                if let temp = self{
+                    temp.titleLabel.frame = CGRect(x: (temp.bounds.size.width - temp.titleLabelWidth)/2, y: 0, width: temp.titleLabelWidth, height: 42)
+                }
+                
+                }, completion: nil)
+            self.progressShape.transform = CATransform3DMakeRotation( CGFloat.pi, 0, 0, 1.0)
+            self.backgroundShape.transform = CATransform3DMakeRotation(CGFloat.pi, 0, 0, 1.0)
+        }
+    }
+    
+    // MARK: - Helper
+    
+    private func rectForShape() -> CGRect {
+        return bounds.insetBy(dx: lineWidth / 2.0, dy: lineWidth / 2.0)
+    }
+    private func pathForShape(rect: CGRect) -> UIBezierPath {
+        let startAngle:CGFloat!
+        let endAngle:CGFloat!
+        
+        if clockwise{
+            startAngle = CGFloat(spaceDegree * .pi / 180.0) + (0.5 * .pi)
+            endAngle = CGFloat((360.0 - spaceDegree) * (.pi / 180.0)) + (0.5 * .pi)
+        }else{
+            startAngle = CGFloat((360.0 - spaceDegree) * (.pi / 180.0)) + (0.5 * .pi)
+            endAngle = CGFloat(spaceDegree * .pi / 180.0) + (0.5 * .pi)
+        }
+        let path = UIBezierPath(arcCenter: CGPoint(x: rect.midX, y: rect.midY), radius: rect.size.width / 2.0, startAngle: startAngle, endAngle: endAngle
+            , clockwise: clockwise)
+    
+        return path
+    }
+}
+
+class DiskStatus {
+
+    //MARK: Formatter MB only
+    class func MBFormatter(_ bytes: Int64) -> String {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = ByteCountFormatter.Units.useMB
+        formatter.countStyle = ByteCountFormatter.CountStyle.decimal
+        formatter.includesUnit = false
+        return formatter.string(fromByteCount: bytes) as String
+    }
+
+
+    //MARK: Get String Value
+    class var totalDiskSpace:String {
+        get {
+            return ByteCountFormatter.string(fromByteCount: totalDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.binary)
+        }
+    }
+
+    class var freeDiskSpace:String {
+        get {
+            return ByteCountFormatter.string(fromByteCount: freeDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.binary)
+        }
+    }
+
+    class var usedDiskSpace:String {
+        get {
+            return ByteCountFormatter.string(fromByteCount: usedDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.binary)
+        }
+    }
+
+
+    //MARK: Get raw value
+    class var totalDiskSpaceInBytes:Int64 {
+        get {
+            do {
+                let systemAttributes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory() as String)
+                let space = (systemAttributes[FileAttributeKey.systemSize] as? NSNumber)?.int64Value
+                return space!
+            } catch {
+                return 0
+            }
+        }
+    }
+
+    class var freeDiskSpaceInBytes:Int64 {
+        get {
+            do {
+                let systemAttributes = try FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory() as String)
+                let freeSpace = (systemAttributes[FileAttributeKey.systemFreeSize] as? NSNumber)?.int64Value
+                return freeSpace!
+            } catch {
+                return 0
+            }
+        }
+    }
+
+    class var usedDiskSpaceInBytes:Int64 {
+        get {
+            let usedSpace = totalDiskSpaceInBytes - freeDiskSpaceInBytes
+            return usedSpace
+        }
+    }
+
+}
+
+
+extension UIDevice {
+    func MBFormatter(_ bytes: Int64) -> String {
+        let formatter = ByteCountFormatter()
+        formatter.allowedUnits = ByteCountFormatter.Units.useMB
+        formatter.countStyle = ByteCountFormatter.CountStyle.decimal
+        formatter.includesUnit = false
+        return formatter.string(fromByteCount: bytes) as String
+    }
+
+    //MARK: Get String Value
+    var totalDiskSpaceInGB:String {
+        var gbString = ByteCountFormatter.string(fromByteCount: totalDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.decimal)
+        gbString = gbString.replacingOccurrences(of: " GB", with: "").replacingOccurrences(of: ",", with: ".")
+        print("Gb string: \(gbString)")
+        return "\(Int(Double(gbString)!.rounded())) GB"
+    }
+
+    var freeDiskSpaceInGB:String {
+        return ByteCountFormatter.string(fromByteCount: freeDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.decimal)
+    }
+
+    var usedDiskSpaceInGB:String {
+        return ByteCountFormatter.string(fromByteCount: usedDiskSpaceInBytes, countStyle: ByteCountFormatter.CountStyle.decimal)
+    }
+
+    var totalDiskSpaceInMB:String {
+        return MBFormatter(totalDiskSpaceInBytes)
+    }
+
+    var freeDiskSpaceInMB:String {
+        return MBFormatter(freeDiskSpaceInBytes)
+    }
+
+    var usedDiskSpaceInMB:String {
+        return MBFormatter(usedDiskSpaceInBytes)
+    }
+
+    //MARK: Get raw value
+    var totalDiskSpaceInBytes:Int64 {
+        guard let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory() as String),
+            let space = (systemAttributes[FileAttributeKey.systemSize] as? NSNumber)?.int64Value else { return 0 }
+        return space
+    }
+
+    /*
+     Total available capacity in bytes for "Important" resources, including space expected to be cleared by purging non-essential and cached resources. "Important" means something that the user or application clearly expects to be present on the local system, but is ultimately replaceable. This would include items that the user has explicitly requested via the UI, and resources that an application requires in order to provide functionality.
+     Examples: A video that the user has explicitly requested to watch but has not yet finished watching or an audio file that the user has requested to download.
+     This value should not be used in determining if there is room for an irreplaceable resource. In the case of irreplaceable resources, always attempt to save the resource regardless of available capacity and handle failure as gracefully as possible.
+     */
+    var freeDiskSpaceInBytes:Int64 {
+        if #available(iOS 11.0, *) {
+            if let space = try? URL(fileURLWithPath: NSHomeDirectory() as String).resourceValues(forKeys: [URLResourceKey.volumeAvailableCapacityForImportantUsageKey]).volumeAvailableCapacityForImportantUsage {
+                return space ?? 0
+            } else {
+                return 0
+            }
+        } else {
+            if let systemAttributes = try? FileManager.default.attributesOfFileSystem(forPath: NSHomeDirectory() as String),
+            let freeSpace = (systemAttributes[FileAttributeKey.systemFreeSize] as? NSNumber)?.int64Value {
+                return freeSpace
+            } else {
+                return 0
+            }
+        }
+    }
+
+    var usedDiskSpaceInBytes:Int64 {
+       return totalDiskSpaceInBytes - freeDiskSpaceInBytes
+    }
+
+}
