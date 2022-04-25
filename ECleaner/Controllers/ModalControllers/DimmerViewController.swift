b66c4d22b0e49c10ee4b88d4ddffb24d4bc98d37
‘oleksiiswift’ <oleksii.codingKey@gmail.com>
2022-04-25 10:41:03 +0300

    EC-93 layout for device

---
+++ ECleaner/Controllers/ModalControllers/DimmerViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DimmerViewController.swift
+//  ECleaner
+//
+//  Created by alexey sorochan on 24.04.2022.
+//
+
+import UIKit
+
+class DimmerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+		self.view.backgroundColor = .red
+    }
+}
